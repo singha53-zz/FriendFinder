522d472e69920f0e503bed0288438b31226d3d87
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2018-09-10 11:57:51 -0400

    set up express

---
+++ server.js
@@ -0,0 +1,23 @@
+var bodyParser = require('body-parser');
+var express = require("express");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.use(bodyParse.urlencoded({ extended: false }))
+
+app.use(body.Parse.json())
+
+app.use((req, res) => {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
+
+app.listen(PORT, () => {
+  console.log('App listenting on PORT:' + PORT)
+})
