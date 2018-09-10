a88c3f09c42f7f3580fcbf23ae6f11073416a976
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2018-09-10 12:11:36 -0400

    add code for html routes and html pages

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,12 @@
+var path = require('path');
+
+module.exports = app => {
+  app.get('/survey', (req, res) => {
+    res.sendFile(path.join(__dirname + '../public/survey.html'));
+  })
+
+  // if no other path is define send user to home page
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname + '../public/home.html'));
+  })
+}
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var bodyParser = require('body-parser');
 var express = require("express");
+var path = require('path');
 
 var app = express();
 var PORT = process.env.PORT || 8080;
